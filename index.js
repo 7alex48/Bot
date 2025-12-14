f014d65a8c4adaf81a79783e3f57981543b7f177
7alex48 <aexshop1@gmail.com>
2025-12-14 18:31:43 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Prihlásený ako ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.reply('Pong 🏓');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
